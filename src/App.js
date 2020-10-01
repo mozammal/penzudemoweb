633b559de7e1c0c5049dde6cab91ca4ffbc22fb2
mozammal <mozammaltomal.1001@gmail.com>
2020-10-01 15:06:16 +0200

    production push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {
+    ApolloClient,
+    ApolloProvider,
+    createHttpLink,
+    InMemoryCache
+} from '@apollo/client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {setContext} from 'apollo-link-context';
+
+import Pages from './pages'
+import GlobalStyle from './components/GlobalStyle';
+
+require('dotenv').config()
+const uri = process.env.REACT_APP_API_URI
+const httpLink = createHttpLink({uri})
+const cache = new InMemoryCache()
+
+const authLink = setContext((_, {headers}) => {
+    return {
+        headers: {
+            ...headers,
+            authorization: localStorage.getItem('token') || ''
+        }
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache,
+    resolvers: {},
+    connectToDevTools: true
+});
+
+const data = {
+    isLoggedIn: !!localStorage.getItem('token')
+}
+
+cache.writeData({data})
+client.onResetStore(() => cache.writeData({data}))
+
+const App = () => {
+    return (
+        <ApolloProvider client={client}>
+            <GlobalStyle/>
+            <Pages/>
+        </ApolloProvider>
+    )
 }
 
 export default App;
